7e0052925658486c51b6b9f9b2da2fd5c90b82c9
Kirill <92873054+Granzh@users.noreply.github.com>
2022-02-24 11:29:30 +0300

    finally

--- js-exercise 2/scroipt.js
+++ js-exercise 2/scroipt.js
@@ -1,22 +1,15 @@
-let array = [];
-let array1 = [];
-let str = "";
-let str1 = "";
-let result = "";
 let word = prompt("Enter the world", "");
 let target = ["e", "u", "o", "a", "i",];
-
+let str = "";
+let str1 = "";
+let result;
 for (let i = 0; i < word.length; i++) {
     let letter = target.includes(word[i]);
-    if (letter) {array.push(word[i])
-    } else {array1.push(word[i]);
+    if (letter) {
+        str += word[i];
+    } else {
+        str1 += word[i];
     }
-}
-for (let i = 0; i < array.length; i++) {
-    str += array[i];
-}
-for (let i = 0; i < array1.length; i++) {
-    str1 += array1[i];
 }
 result = str + str1;
 alert(result);
