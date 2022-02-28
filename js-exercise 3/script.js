89ece104ff528d6a67a33d604a453e83bf7c373b
Kirill <92873054+Granzh@users.noreply.github.com>
2022-02-28 20:35:22 +0300

    :\

--- js-exercise 3/script.js
+++ js-exercise 3/script.js
@@ -1,24 +1,24 @@
 let str = prompt('Введите числа', '');
 let re = /\s*,\s*/;
-let D = 0;
-let Result;
-let Numbers = str.split(re);
-for (let i = 0; i < Numbers.length; i++) {
-    if ((Numbers[i] > 0) && (Numbers[i] < 100)) {
-        D += Number(Numbers[i]);
+let d = 0;
+let result;
+let numbers = str.split(re);
+for (let i = 0; i < numbers.length; i++) {
+    if ((numbers[i] > 0) && (numbers[i] < 100)) {
+        d += Number(numbers[i]);
     }
 }
-if (Numbers.length > 0) {
-    Result = D/Numbers.length;
+if (numbers.length > 0) {
+    result = d/numbers.length;
 }
-if (Result < 60) {
+if (result < 60) {
     alert('F')
-} else if (Result < 70) {
+} else if (result < 70) {
     alert('D')
-} else if (Result < 80) {
+} else if (result < 80) {
     alert('C')
-} else if (Result < 90) {
+} else if (result < 90) {
     alert('B')
-} else if (Result < 100) {
+} else if (result < 100) {
     alert('A')
 }
